f6f31de634d623d5a14195eef7a5a1909d6e90c0
USER <1905707@kiit.ac.in>
2022-11-15 00:38:46 +0530

    Added the beans for base URLs for users, solutions and issues APIs...

--- src/main/java/com/api/api_gateway/ConfigClass.java
+++ src/main/java/com/api/api_gateway/ConfigClass.java
@@ -1,5 +1,6 @@
 package com.api.api_gateway;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,9 +8,32 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class ConfigClass
 {
+    @Value("${api.baseurl.issues}")
+    private String issueBaseUrl;
+
+    @Value("${api.baseurl.users}")
+    private String usersBaseUrl;
+
+    @Value("${api.baseurl.solutions}")
+    private String solutionsBaseUrl;
     @Bean
     public WebClient getWebClient()
     {
         return WebClient.create();
+    }
+
+    @Bean(name = "issuesBaseUrl")
+    public String getIssueBaseUrl(){
+        return issueBaseUrl;
+    }
+
+    @Bean(name = "usersBaseUrl")
+    public String getUsersBaseUrl(){
+        return usersBaseUrl;
+    }
+
+    @Bean(name = "solutionsBaseUrl")
+    public String getSolutionsBaseUrl(){
+        return solutionsBaseUrl;
     }
 }
