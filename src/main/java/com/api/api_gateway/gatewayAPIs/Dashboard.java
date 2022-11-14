7868e851f899d088198fe6294db5e79989ef5505
USER <1905707@kiit.ac.in>
2022-11-14 21:01:54 +0530

    Updated 'getInitialDashboardData' method and added 'getNewPostFromUserAndSave' method...

--- src/main/java/com/api/api_gateway/gatewayAPIs/Dashboard.java
+++ src/main/java/com/api/api_gateway/gatewayAPIs/Dashboard.java
@@ -2,20 +2,26 @@ package com.api.api_gateway.gatewayAPIs;
 
 import com.api.api_gateway.models.DashboardData;
 import com.api.api_gateway.models.Issues;
+import com.api.api_gateway.models.ResponseObject;
 import com.api.api_gateway.models.UserProfile;
 import com.api.api_gateway.services.IssuesServices;
 import com.api.api_gateway.services.UserServices;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/dashboard")
 public class Dashboard
 {
+    private final IssuesServices issuesServices;
+    private final UserServices userServices;
+
+    public Dashboard(IssuesServices issuesServices, UserServices userServices) {
+        this.issuesServices = issuesServices;
+        this.userServices = userServices;
+    }
+
     @GetMapping("/")
     public ResponseEntity<DashboardData> getInitialDashboardData(
             @RequestHeader(name = "uid") String userID
@@ -23,12 +29,12 @@ public class Dashboard
     {
         HttpStatus status = HttpStatus.OK;
 
-        UserProfile userProfile = UserServices.getUserProfile(userID); //getting the user profile data
-        Issues[] issues = IssuesServices.getIssuesList(userID,null); //getting the issues of the user
+        ResponseObject<UserProfile> userProfile = userServices.getUserProfile(userID); //getting the user profile data
+        ResponseObject<Issues[]> issues = issuesServices.getIssuesList(userID,null); //getting the issues of the user
 
         DashboardData dashboardData;
 
-        if(userProfile==null || issues==null)
+        if(userProfile.getStatus()!=HttpStatus.OK || issues.getStatus()!=HttpStatus.OK)
         {
             dashboardData = null;
             status = HttpStatus.NOT_FOUND;
@@ -36,10 +42,20 @@ public class Dashboard
         else
         {
             dashboardData = new DashboardData();
-            dashboardData.setUserProfile(userProfile);
-            dashboardData.setIssues(issues);
+            dashboardData.setUserProfile(userProfile.getMessageBody());
+            dashboardData.setIssues(issues.getMessageBody());
         }
 
         return new ResponseEntity<>(dashboardData, status);
+    }
+
+    @PostMapping("/compose/new")
+    public ResponseEntity<Object> getNewPostFromUserAndSave(@RequestBody Issues newPost)
+    {
+        ResponseObject<Issues> response = issuesServices.saveNewPost(newPost);
+        return new ResponseEntity<>(
+                response.getMessageBody(),
+                response.getStatus()
+        );
     }
 }
