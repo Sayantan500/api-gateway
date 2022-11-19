2cddf31374d3b7b88adfbb41b98ff9d25ec53e42
USER <1905707@kiit.ac.in>
2022-11-20 00:24:52 +0530

    Updated the data model...

--- src/main/java/com/api/api_gateway/models/LoginResponse.java
+++ src/main/java/com/api/api_gateway/models/LoginResponse.java
@@ -4,7 +4,9 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class LoginResponse
 {
-    @JsonProperty("api_key")  private String token;
+    @JsonProperty("api_key")        private String token;
+    @JsonProperty("uid")            private String uid;
+    @JsonProperty("accessToken")    private String accessToken;
 
     public LoginResponse() {}
 
@@ -14,5 +16,21 @@ public class LoginResponse
 
     public void setToken(String token) {
         this.token = token;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
     }
 }
