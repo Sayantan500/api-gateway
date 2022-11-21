3e219b78b013cf7ace7659b0ecd515d64181cb47
USER <1905707@kiit.ac.in>
2022-11-21 12:16:43 +0530

    Updated the data model by removing the api_token property...

--- src/main/java/com/api/api_gateway/models/LoginResponse.java
+++ src/main/java/com/api/api_gateway/models/LoginResponse.java
@@ -4,19 +4,10 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class LoginResponse
 {
-    @JsonProperty("api_key")        private String token;
     @JsonProperty("uid")            private String uid;
     @JsonProperty("accessToken")    private String accessToken;
 
     public LoginResponse() {}
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
 
     public String getUid() {
         return uid;
