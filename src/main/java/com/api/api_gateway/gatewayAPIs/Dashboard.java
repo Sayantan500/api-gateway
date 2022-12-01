11570cb31ad0c5c51f52cf1272b3a8883cb57e53
d707_sayantan <sayantandas657@gmail.com>
2022-12-01 23:23:09 +0530

    Allowed cors from all origin...

--- src/main/java/com/api/api_gateway/gatewayAPIs/Dashboard.java
+++ src/main/java/com/api/api_gateway/gatewayAPIs/Dashboard.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/dashboard")
+@CrossOrigin("*")
 public class Dashboard
 {
     private final IssuesServices issuesServices;
